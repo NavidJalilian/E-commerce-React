90ed81916aa4ead1caacd614ede7840b721c4ffc
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-06-11 01:58:25 +0430

    adding notif section to CategorySection items (add to cart indicator)

--- src/components/CategorySection.js
+++ src/components/CategorySection.js
@@ -1,18 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
 import { CartContext } from "../contexts/cartContext";
+import "./CategorySection.css";
 
-export default function CategorySection({product}) {
+export default function CategorySection({ product }) {
   const { addItemToCart } = useContext(CartContext);
   const addProductToCart = () => addItemToCart(product);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
   const { id, name, imageUrl, price } = product;
+
+  const handleAddToCart = (product) => {
+    addProductToCart(product);
+    setIsAddedToCart(true);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsAddedToCart(false), 1000);
+    return () => clearTimeout(timer);
+  });
+  
+  const notifCssClass = isAddedToCart && "noift-tab-navbar";
 
   return (
     <div className="shop-card" key={id}>
       <div className="shop-image">
         <img src={imageUrl} alt={name} />
-        <button className="btn btn-inverted" onClick={addProductToCart}>
+        <button className="btn btn-inverted" onClick={handleAddToCart}>
           add to cart
         </button>
+        <div className={"success-tab " + notifCssClass}>
+          <span className="success-tab-check">&#10004;</span>
+          <span className="shop-success-title ">Product added to cart</span>
+        </div>
       </div>
       <div className="shop-card-content">
         <span>{name}</span>
